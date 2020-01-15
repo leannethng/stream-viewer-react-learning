998941a4bceada29b7f415d6724fc050397b441c
Leanne <leanne.thng@gmail.com>
2020-01-14 20:11:41 -0800

    Made the Nav bar highlight the current page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+
+
 
 function Header(){
   return(
     <nav className="navbar justify-content-center ">
       <li className="nav-item nav-link ">
-        <Link className=" nav-style" to="/">Top Games</Link>
+        <NavLink className=" nav-style" exact to="/" activeClassName="active">Top Games</NavLink>
       </li>
       <li className="nav-item nav-link ">
-        <Link className="nav-style" to="/top-streams">Top Live Streams</Link>
+        <NavLink className="nav-style" to="/top-streams" activeClassName="active">Top Live Streams</NavLink>
       </li>
     </nav>
   );
 }
+
 
 export default Header;
